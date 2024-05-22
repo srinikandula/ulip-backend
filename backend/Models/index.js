f2bcaead5fb7048dff1ad4508095b83eec4ed48a
harikrishna766 <harikrishna.pulivarthi@whizzard.in>
2024-05-22 16:03:08 +0530

    add .evn

--- backend/Models/index.js
+++ backend/Models/index.js
@@ -5,8 +5,20 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const dotenv = require('dotenv');
+dotenv.config();
+// const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+
+const config = {
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  dialect: process.env.DATABASE_DIALECT,
+  // Other configuration options as needed
+};
+
 
 let sequelize;
 if (config.use_env_variable) {
